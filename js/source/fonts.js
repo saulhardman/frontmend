5e34a87a07738a73a08aaf8d6d174acca11ce60e
Saul Hardman <hello@iamsaul.co.uk>
2013-02-08 02:25:01 +0000

    Testing pre commit hook

--- js/source/fonts.js
+++ js/source/fonts.js
@@ -1,10 +1,6 @@
 define(function () {
     if (config.typekit !== undefined && config.typekit !== "XXXXX") {
-        var html = document.getElementsByTagName("html")[0],
-            timeout = setTimeout(function () {
-                html.className = html.className.replace(/(\s|^)wf-loading(\s|$)/g, " ");
-                html.className += " wf-inactive";
-            }, 3000);
+        var timeout = setTimeout(fontsInactive(true), 3000);
         requirejs(["typekit"], function () {
             try {
                 Typekit.load();
@@ -14,5 +10,14 @@ define(function () {
             clearTimeout(timeout);
             console.log("Fonts loaded.");
         });
+    } else {
+        fontsInactive(false);
+    }
+    function fontsInactive (set) {
+        var html = document.getElementsByTagName("html")[0];
+        html.className = html.className.replace(/(\s|^)wf-loading(\s|$)/g, " ");
+        if (set) {
+            html.className += " wf-inactive";
+        }
     }
 });
